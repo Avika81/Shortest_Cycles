9f23801feca228d0fc3154d81a0bc45fc2354d95
Avi_Kadria <avi.kadria3@gmail.com>
2017-11-29 14:56:49 +0200

    runned the finding min over the night on some graphs, with both random functions.

--- Undirected_unweighted_graphs.py
+++ Undirected_unweighted_graphs.py
@@ -2,10 +2,11 @@ import snap
 import numpy
 import queue
 import random
+import math
 
 
 def find_shortest_cycle(Graph):
-    best = [999999999999999999999999999999, -1]
+    best = [float("nan"), -1]
     for node in Graph.Nodes():
         is_seen = numpy.zeros(Graph.GetNodes())
         is_seen[node.GetId()] = 1
@@ -28,29 +29,37 @@ def find_shortest_cycle(Graph):
                     vert_queue.append([Graph.GetNI(neighbor), curr_vertex])
                 elif is_seen[neighbor]:  # distance > 2 because we don't want x->y->x cycles
                     result = [distance + is_seen[neighbor], neighbor]
-                    if result[0] < best[0]:
+                    if result[0] < best[0] or math.isnan(best[0]):
                         best = result
-    print "length :" + str(best[0]) + "\tstart :" + str(best[1])
+    if math.isnan(best[0]):
+        print "There is no cycle in this graph!"
+    else:
+        print "length : " + str(best[0]) + "\tstart : " + str(best[1])
 
 
 def create_random_graph(num_vertices, p):
-    print "started the random graph"
     # generates the empty graph with n vertices:
     Graph = snap.TUNGraph.New()
     for i in range(num_vertices):
         Graph.AddNode(i)
+
     for i in range(num_vertices):
         for j in range(num_vertices):
             if random.random() < p:
                 Graph.AddEdge(i, j)
-    print "finished creating the random graph!:"
-    print "p = " + str(p) + "\tnumber of edges = " + str(Graph.GetEdges()) + "\tEstimate was : " + str((num_vertices**2)/p)
+    print "num_vertices = " + str(num_vertices) + "\tp = " + str(p) + "\tnumber of edges = " + str(Graph.GetEdges()) + "\tEstimate was : " + str((num_vertices**2)*p)
 
     #Graph.Dump()
     return Graph
 
-TempGraph = create_random_graph(100, 0.01)
-# TempGraph = snap.GenRndGnm(snap.PUNGraph, 100, 100)  # generates random graph (type, num odes, num edges
+for num_vertices in range(100, 1001, 100):
+    for power in numpy.arange(0.1, 2, 0.05):
+        print "m =  n ^ " + str(power)
+        print "num vertices = " + str(num_vertices)
+        TempGraph = snap.GenRndGnm(snap.PUNGraph, num_vertices, int(num_vertices**power))  # generates random graph (type, num odes, num edges
+        # TempGraph = create_random_graph(num_vertices, num_vertices**(power-2))
+        find_shortest_cycle(TempGraph)
+        print "---------------------------------------------------------------------------------------------"    # delimiter
 
-find_shortest_cycle(TempGraph)
+
 # TempGraph.Dump()
